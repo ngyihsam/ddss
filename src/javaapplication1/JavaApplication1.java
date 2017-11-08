0c9c9ba8d8d3632d22d24248aacfa40a2282a96e
B1 <B1@B144>
2017-11-08 13:07:53 +0800

    [Finishes #152665857]

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,6 +16,7 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         System.out.println("week4");
+        System.out.println("weekasasaas");
     }
     
 }
